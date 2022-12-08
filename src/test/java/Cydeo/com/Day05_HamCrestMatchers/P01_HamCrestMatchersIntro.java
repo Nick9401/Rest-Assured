a510b536a9d4dae3f7d56e8a27798a95877d59cf
Nicolae Sultan <sultan.nicolae1994@gmail.com>
2022-12-08 11:23:26 -0500

    klJ

--- src/test/java/Cydeo/com/Day05_HamCrestMatchers/P01_HamCrestMatchersIntro.java
+++ src/test/java/Cydeo/com/Day05_HamCrestMatchers/P01_HamCrestMatchersIntro.java
@@ -7,6 +7,9 @@ import static org.hamcrest.Matcher.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
@@ -65,22 +68,48 @@ public class P01_HamCrestMatchersIntro {
         // it will read data from response under the hood
 
 
-        String msg="API is fun!";
+        String msg = "API is fun!";
 
-        assertThat(msg,is("API is fun!"));
-        assertThat(msg,equalTo("API is fun!"));
-        assertThat(msg,equalToIgnoringCase("api is fun!"));
+        assertThat(msg, is("API is fun!"));
+        assertThat(msg, equalTo("API is fun!"));
+        assertThat(msg, equalToIgnoringCase("api is fun!"));
 
-        assertThat(msg,startsWith("API"));
-        assertThat(msg,startsWithIgnoringCase("api"));
+        assertThat(msg, startsWith("API"));
+        assertThat(msg, startsWithIgnoringCase("api"));
 
-        assertThat(msg,endsWith("fun!"));
-        assertThat(msg,endsWithIgnoringCase("FUN!"));
+        assertThat(msg, endsWith("fun!"));
+        assertThat(msg, endsWithIgnoringCase("FUN!"));
 
-        assertThat(msg,containsString("is"));
-        assertThat(msg,containsStringIgnoringCase("IS"));
+        assertThat(msg, containsString("is"));
+        assertThat(msg, containsStringIgnoringCase("IS"));
 
-        assertThat(msg,not("Fun!"));
-        assertThat(msg,is(not("Fun!")));
+        assertThat(msg, not("Fun!"));
+        assertThat(msg, is(not("Fun!")));
     }
+        @Test
+        public void testCollections() {
+
+            List<Integer> numberLst = Arrays.asList( 3, 5, 1, 77, 44, 76 ) ; // 6 elements here
+
+            // how to check size of elements
+            assertThat(numberLst,hasSize(6));
+
+            // how to check 44 is into collection
+            assertThat(numberLst,hasItem(44));
+            // assertThat(numberLst,hasItem(2));  to make it fail
+
+            // how to check 44 and 76 is into collection
+            assertThat(numberLst,hasItems(44,76));
+            assertThat(numberLst,hasItems(44,76,1));
+            // assertThat(numberLst,hasItems(44,76,1,2)); to make it fail
+
+            assertThat(numberLst,hasItems(greaterThan(70)));
+
+
+            //everyItem --> check each element into array about realted condition
+            assertThat(numberLst,everyItem(greaterThanOrEqualTo(1)));
+
+            assertThat(numberLst,containsInRelativeOrder(3, 5, 1, 77, 44, 76));
+
+        }
 }
