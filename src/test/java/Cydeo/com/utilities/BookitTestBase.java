efc1117362667d9c4deced9d2c677cc9a763eeb1
Nicolae Sultan <sultan.nicolae1994@gmail.com>
2022-12-13 15:22:53 -0500

    o

--- src/test/java/Cydeo/com/utilities/BookitTestBase.java
+++ src/test/java/Cydeo/com/utilities/BookitTestBase.java
@@ -3,11 +3,10 @@ package Cydeo.com.utilities;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 
-public class BookTestBase {
-
+public class BookitTestBase {
     @BeforeAll
     public static void init() {
 
         RestAssured.baseURI = "https://api.qa.bookit.cydeo.com";
     }
-    }
+}
